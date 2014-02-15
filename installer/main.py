a5f69cef76f3685853aa32f5bedd31297d1a5e1c
Meneth <github@meneth.com>
2014-02-16 00:40:05 +0100

    Replaced all raw_input

--- installer/main.py
+++ installer/main.py
@@ -42,7 +42,7 @@ try:
     SWMHversion = open("modules/SWMH/version.txt").readline().strip()
     NBRTversion = open("modules/NBRT+/version.txt").readline().strip()
     ARKOversion = open("modules/ARKOpack_Armoiries/version.txt").readline()
-    language = raw_input("For English, press enter. En francais, taper 'f'. Para espanol, presiona 'e'.").lower()
+    language = promptUser("For English, press enter. En francais, taper 'f'. Para espanol, presiona 'e'.").lower()
     if language == "f":
         print("Cette version de Historical Immersion Project date du %s" % version)
         print("Taper 'y' ou 'oui' pour valider, ou laisser le champ vierge. Toute autre reponse sera interpretee comme un non.")
@@ -53,26 +53,26 @@ try:
         print("This version of the Historical Immersion Project was released %s" % version)
         print("Type 'y' or 'yes' for yes, or leave the field blank. Anything else will be interpreted as 'no'.")
     if language == "f":
-        move = raw_input("Deplacer les fichiers plutot que les copier est bien plus rapide, mais rend l'installation de plusieurs copies du mod plus difficile. \n"
+        move = promptUser("Deplacer les fichiers plutot que les copier est bien plus rapide, mais rend l'installation de plusieurs copies du mod plus difficile. \n"
                          "Voulez-vous que les fichiers soient deplaces plutot que copies ? [oui]").lower()
     elif language == "e":
-        move = raw_input("Mover los archivos en lugar de copiarlos es mucho mas rapido, pero hace que la instalacion de varias copias sea mas complicada.\n"
+        move = promptUser("Mover los archivos en lugar de copiarlos es mucho mas rapido, pero hace que la instalacion de varias copias sea mas complicada.\n"
                          "Quieres que los archivos de los modulos se muevan en lugar de copiarse? [si]").lower()
     else:
-        move = raw_input("Moving the files instead of copying them is much quicker, but makes installing several copies more difficult.\n"
+        move = promptUser("Moving the files instead of copying them is much quicker, but makes installing several copies more difficult.\n"
                          "Do you want to have the module files moved rather than copied? [yes]").lower()
     if move == "" or move == "y" or move == "yes" or move == "oui" or move == "o":
         move = True
     else:
         move = False
     if language == "f":
-        targetFolder = raw_input("Installer le mod dans un repertoire existant supprimera ce repertoire. Dans quel repertoire souhaitez-vous proceder a l'installation ?\n"
+        targetFolder = promptUser("Installer le mod dans un repertoire existant supprimera ce repertoire. Dans quel repertoire souhaitez-vous proceder a l'installation ?\n"
                                  "Laissez le champ vierge pour 'Historical Immersion Project'. ")
     elif language == "e":
-        targetFolder = raw_input("Instalar el mod en una carpeta existente eliminara dicha carpeta. En que carpeta deseas realizar la instalacion?\n"
+        targetFolder = promptUser("Instalar el mod en una carpeta existente eliminara dicha carpeta. En que carpeta deseas realizar la instalacion?\n"
                                  "Dejar en blanco para 'Historical Immersion Project'. ")
     else:
-        targetFolder = raw_input("Installing the mod to a folder that exists will delete that folder. What folder would you like to install to?\n"
+        targetFolder = promptUser("Installing the mod to a folder that exists will delete that folder. What folder would you like to install to?\n"
                                  "Leave blank for 'Historical Immersion Project'. ")
     if targetFolder == "":
         targetFolder = "Historical Immersion Project"
@@ -167,11 +167,11 @@ try:
             moveFolder("VIET_Immersion")
             moveFolder("Converter/VIET")
         if language == "f":
-            answer = raw_input("VIET Immersion necessite tous les DLC de portraits. Les avez-vous tous ? [oui]")
+            answer = promptUser("VIET Immersion necessite tous les DLC de portraits. Les avez-vous tous ? [oui]")
         if language == "e":
-            answer = raw_input("VIET inmersion depende de los retratos DLC. Tiene todos los retratos DLC? [si]")
+            answer = promptUser("VIET inmersion depende de los retratos DLC. Tiene todos los retratos DLC? [si]")
         else:
-            answer = raw_input("VIET Immersion depends on the portrait DLC. Do you have all portrait DLC? [yes]")
+            answer = promptUser("VIET Immersion depends on the portrait DLC. Do you have all portrait DLC? [yes]")
         if not (answer == "" or answer == "y" or answer == "yes" or answer == "oui" or answer == "o" or answer == "s" or answer == "si"):
             shutil.rmtree("%s/common/cultures" % targetFolder)
             os.remove("%s/interface/portrait_sprites_DLC.gfx" % targetFolder)
@@ -196,11 +196,11 @@ try:
     with open("%s/version.txt" % targetFolder, "w") as output:
         output.write("".join(moduleOutput))
     if language == "f":
-        raw_input("Installation terminee. Taper entree pour sortir.")
+        promptUser("Installation terminee. Taper entree pour sortir.")
     elif language == "e":
-        raw_input("Instalacion terminada. Presiona Enter para salir.")
+        promptUser("Instalacion terminada. Presiona Enter para salir.")
     else:
-        raw_input("Installation done. Hit enter to exit.")
+        promptUser("Installation done. Hit enter to exit.")
 except:
     print("Error:")
     traceback.print_exc(file=sys.stdout)
