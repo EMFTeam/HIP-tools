e5eb6a850e1364629d13ef0932618eb21972ccb3
Matthew D. Hall <zijistark@gmail.com>
2014-03-04 20:30:37 -0800

    Seemingly working (OK w/ William the Conqueror + lots) input side
    I might fork to a similar but tighter C++ implementation at this point
    and come running back here if that proves even more of a headache.
    
    Python's dynamic binding (and typing) leave a lot to be desired as to
    known vs. unknown points of failure, and history data corruption /
    loss upon rewrite/transformation due to some corner case in the code
    that was never exercised or tested explicitly. Such a problem would be
    totally unacceptable and would be far too easy to skate-by
    unnoticed. I can't test every existing or future valid character
    history language element's translation into the parser without
    manually inspecting and verifying every single character's parse; I
    have to rely upon invariants. Python doesn't feel right for this.
    
    Unfortunately, the C++ version is going to look similar (just
    seemingly more complicated to those unfamiliar w/ C or C++) in
    implementation, as I don't foresee that using normal C++ tools like
    quex/flex and, say, even a hand-coded predictive LL(1) parser is
    necessary or desired for the pure simplicity of this "language."
    
    Will he fork it or won't he?  We'll have to see.

--- culture-rewrite/history.py
+++ culture-rewrite/history.py
@@ -3,7 +3,6 @@ __author__ = 'zijistark'
 import os
 import codecs
 import re
-from collections import deque
 
 INDENT_UNIT = '\t'
 
@@ -17,6 +16,9 @@ p_char_start = re.compile(r'^\s*(\d+)\s*=\s*[{]\s*(#.*)?$')
 p_char_culture = re.compile(r'^\s*culture\s*=\s*(?:"([^"]+)"|([^"\s]+))\s*(#.*)?$')
 p_char_dynasty = re.compile(r'^\s*dynasty\s*=\s*(?:"(\d+)"|(\d+))\s*(#.*)?$')
 p_char_hist_entry_start = re.compile(r'^\s*(\d{1,4})\.(\d{1,2})\.(\d{1,2})\s*=\s*[{]$')
+p_char_hist_entry_birth = re.compile(r'^\s*birth\s*=\s*(?:"(?:[^"]+)"|(?:[^"\s]+))')
+p_open_brace = re.compile(r'^([^}]*)[{]')
+p_close_brace = re.compile(r'^([^{]*)[}]')
 
 class CHParseError(Exception):
     def __init__(self, msg):
@@ -57,6 +59,62 @@ class CHFileCharHistEntry:
         self.date = date
         self.elems = []
 
+    def parse(self, ch, chfc, f, n_line):
+        buf = ''
+        buf_idx = 0
+        nest_level = 1
+        birth = False
+        while nest_level > 0:
+            if len(buf) == buf_idx:
+                buf = f.readline()
+                if len(buf) == 0:  # EOF
+                    raise CHParseError("Unexpected EOF while parsing character ID %d [%s: line %d]!" % (chfc.id,
+                                                                                                        g_filename,
+                                                                                                        n_line))
+                n_line += 1
+                buf_idx = 0  # Reset start pointer to beginning of new line
+                buf = buf.rstrip()
+                continue
+
+            # buf is nonempty, buf[buf_idx:] is also nonempty, and we still having matching/consumption to do.
+
+            # Look for a birth statement at nest_level == 1 in case this history entry includes/is a birth date
+            if not birth and nest_level == 1:
+                m = p_char_hist_entry_birth.match(buf[buf_idx:])
+                if m:
+                    birth = True
+                    assert m.end() > 0
+                    buf_idx += m.end()
+                    continue
+
+            # Look for a starting brace to increment the nest level, capture interim literal data
+            m = p_open_brace.match(buf[buf_idx:])
+            if m:
+                self.elems.append(CHFileLiteral(m.group()))
+                nest_level += 1
+                assert m.end() > 0
+                buf_idx += m.end()
+                continue
+
+            # Look for a closing brace to decrement the nest level, capture interim literal data
+            m = p_close_brace.match(buf[buf_idx:])
+            if m:
+                self.elems.append(CHFileLiteral(m.group()))
+                nest_level -= 1
+                assert m.end() > 0
+                buf_idx += m.end()
+                continue
+
+            # Else, it's just literal data/effects, so capture all the remainder
+            self.elems.append(buf[buf_idx:])
+            buf_idx = len(buf)  # One past end, fully consumed
+
+        # nest_level == 0, return input file line number and whether we included a birth date/effect in this entry
+        if buf_idx < len(buf):
+            raise CHParseError("Out-of-place trailing characters after character history entry closing brace ["
+                               "%s:L%d]!" % (g_filename, n_line))
+        return (n_line, birth)
+
 
 class CHFileChar:
     def __init__(self, id, comment=None):
@@ -66,22 +124,26 @@ class CHFileChar:
         self.hist_entries = []
         self.elems = []
 
-    def finalize(self):
+
+    def _finalize(self):
         self.dynasty = getattr(self, 'dynasty', CommentableVal(0))  # Default to lowborn dynasty = 0
         if not hasattr(self, 'culture'):
             raise CHParseError('Character ID %d [%s: line %d] has no culture defined!'
-                               % (self.id, self.chf.filename, self.start_line))
+                               % (self.id, g_filename, self.start_line))
+        if not hasattr(self, 'bdate'):
+            raise CHParseError('Character ID %d [%s: line %d] has no birth date defined!'
+                               % (self.id, g_filename, self.start_line))
 
-    def parse(self, ch, chf, f, n_line):
+
+    def parse(self, ch, f, n_line):
         self.start_line = n_line
-        nest_level = 0
-        ch.log_dbg("Parsing character ID %d [%s:L%d] ..." % (self.id, chf.filename, n_line))
+        ch.log_dbg("Parsing character ID %d [%s:L%d] ..." % (self.id, g_filename, n_line))
         while True:
             line = f.readline()
             if len(line) == 0:  # EOF
                 raise CHParseError("Unexpected EOF while parsing character ID %d [%s: line %d]!" % (self.id,
-                                                                                             self.chf.filename,
-                                                                                             n_line))
+                                                                                                    g_filename,
+                                                                                                    n_line))
             n_line += 1
             line = line.rstrip()
 
@@ -98,16 +160,21 @@ class CHFileChar:
             m = p_char_hist_entry_start.match(line)
             if m:
                 d = DateVal(m.group(1), m.group(2), m.group(3))
-                self.hist_entries.append(CHFileCharHistEntry(d))
-                nest_level = 1
-
-
+                h_ent = CHFileCharHistEntry(d)
+                self.hist_entries.append(h_ent)
+                (n_line, is_birth) = h_ent.parse(ch, self, f, n_line)
+                if is_birth:
+                    self.bdate = d
+                continue
 
             m = p_char_end.match(line)
             if m:
-                self.finalize()
-                ch.log_dbg(str(self.id) + " {\n" + ' dynasty => ' + str(self.dynasty.val) + "\n" + " culture => "
-                           + self.culture.val + "\n}\n")
+                self._finalize()
+                ch.log_dbg(str(self.id) + " {\n"
+                           + ' dynasty  => ' + str(self.dynasty.val) + "\n"
+                           + ' culture  => ' + self.culture.val + "\n"
+                           + ' birthday => ' + str(self.bdate) + "\n"
+                           + "}\n")
                 return n_line
 
             # Upon no match, record the line as literal data (we don't support all possible character history
@@ -125,7 +192,9 @@ class CHFile:
         self.elems = []
 
     def parse(self):
-        self.ch.log_info("Parsing file '%s' ..." % self.filename)
+        global g_filename
+        g_filename = self.filename
+        self.ch.log_info("Parsing file '%s' ..." % g_filename)
         n_line = 0
         with codecs.open(self.path, encoding='cp1252') as f:
             while True:
@@ -144,8 +213,8 @@ class CHFile:
 
                     id = int(m.group(1))
                     # TODO: check for duplicate ID
-                    c = CHFileChar(self.ch, self, id, comment)
-                    n_line = c.parse(f, n_line)
+                    c = CHFileChar(id, comment)
+                    n_line = c.parse(self.ch, f, n_line)
                     self.elems.append(c)
                     continue
 
