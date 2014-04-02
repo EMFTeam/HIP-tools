c9ff69694b65f98dc2bd02107d0faf321495e2e0
Matthew D. Hall <zijistark@gmail.com>
2014-04-02 00:40:25 -0700

    Handled preexistence of output history folder with --force semantics

--- culture-rewrite/culture-rewrite.py
+++ culture-rewrite/culture-rewrite.py
@@ -5,6 +5,7 @@ VERSION = '0.1.0'
 
 import os
 import sys
+import shutil
 import traceback
 import argparse
 import history
@@ -42,15 +43,30 @@ def main():
     try:
         # TODO: check input file exists and open cleanly
 
+
+        # Handle output directory preexistence
+        if os.path.exists(args.output_history_dir):
+            if args.force:
+                if os.path.isdir(args.output_history_dir):
+                    shutil.rmtree(args.output_history_dir)
+                else:
+                    os.remove(args.output_history_dir)
+            else:
+                sys.stderr.write("The output directory already exists (use -f / --force to overwrite by default): " +
+                                 args.output_history_dir + "\n")
+                return 1
+
+        # Create a new output directory
+        os.makedirs(args.output_history_dir)
+
         char_hist = history.CharHistory(sys.stdout, args.verbose)
         char_hist.parse_dir(args.history_dir)
-        char_hist.rewrite()
 
         return 0
 
     except history.CHParseError as e:
         sys.stderr.write('\nFatal character history parse error:\n' + str(e))
-        return 1
+        return 2
 
     except:
         sys.stderr.write('\nUnexpected fatal error occurred! Stack trace:\n\n')
