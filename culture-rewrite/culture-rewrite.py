701a9c6e4175c4d1e586fcf8d55fa44f27038d89
Matthew D. Hall <zijistark@gmail.com>
2014-04-02 21:16:15 -0700

    CSV reader wrapper added; CSV spreadsheet dialect auto-detection

--- culture-rewrite/culture-rewrite.py
+++ culture-rewrite/culture-rewrite.py
@@ -9,6 +9,7 @@ import shutil
 import re
 import traceback
 import argparse
+import csv
 import history
 
 
@@ -19,7 +20,7 @@ def get_args():
     parser = argparse.ArgumentParser(
         description="Split, merge, & melt cultures in a CKII character history database [SWMH German target].",
     )
-    parser.add_argument('input-file', metavar='FILENAME',
+    parser.add_argument('rule_file', metavar='FILENAME',
                         help='name of CSV file containing dynasty and early -> later culture melt rules')
     parser.add_argument('--date', required=True,
                         help='date at which the early -> later cultural transition occurs for characters, '
@@ -46,34 +47,54 @@ def main():
         # Validate the split date
         m = p_date.match(args.date)
         if not m:
-            sys.stderr.write("The given cultural split date '%s' is not a valid CKII-format date or pseudo-date.\n"
+            sys.stderr.write("The given cultural split date '%s' is not a valid CKII-format date (or pseudo-date).\n"
                              % args.date)
             return 1
 
         # Ensure that we can open the input spreadsheet and auto-recognize its CSV dialect
-        if not os.path.isfile(args.input_file):
-            sys.stderr.write("The given culture melt input spreadsheet '%s' is not a valid file.\n" % args.input_file)
+        if not os.path.isfile(args.rule_file):
+            sys.stderr.write("The given culture melt input spreadsheet '%s' is not a valid file.\n" % args.rule_file)
             return 1
 
-        # Handle output directory preexistence
-        if os.path.exists(args.output_history_dir):
-            if args.force:
-                if os.path.isdir(args.output_history_dir):
-                    shutil.rmtree(args.output_history_dir)
+        with open(args.rule_file, 'rb') as f:
+            # Sample file data, auto-detect CSV dialect, reset file pointer to beginning
+            csv_dialect = csv.Sniffer().sniff(f.read(2048), ';,')
+            f.seek(0)
+
+            # Wrap a CSV reader for the detected dialect around the input file
+            csv_reader = csv.reader(f, dialect=csv_dialect)
+
+            csv_header = csv_reader.next()  # Advance past header row
+
+            if csv_header is None:
+                sys.stderr.write("Failed to read CSV spreadsheet header row (corrupt/invalid file?)\n")
+                return 3
+
+            # Handle output directory preexistence
+            if os.path.exists(args.output_history_dir):
+                if args.force:
+                    if os.path.isdir(args.output_history_dir):
+                        shutil.rmtree(args.output_history_dir)
+                    else:
+                        os.remove(args.output_history_dir)
                 else:
-                    os.remove(args.output_history_dir)
-            else:
-                sys.stderr.write("The output directory already exists (use -f / --force to overwrite by default): " +
-                                 args.output_history_dir + "\n")
-                return 1
+                    sys.stderr.write("The output directory already exists (use -f / --force to overwrite by default): " +
+                                     args.output_history_dir + "\n")
+                    return 1
 
-        # Create a new output directory
-        os.makedirs(args.output_history_dir)
+            # Create a new output directory
+            os.makedirs(args.output_history_dir)
 
-        char_hist = history.CharHistory(sys.stdout, args.verbose)
-        char_hist.parse_dir(args.history_dir)  # Parse entire character history folder
+            char_hist = history.CharHistory(sys.stdout, args.verbose)
+            char_hist.parse_dir(args.history_dir)  # Parse entire character history folder
 
-        # TODO: apply transformation rules from spreadsheet to in-memory database
+            # Spreadsheet field indices of interest
+            DYN = 0
+            CUL_EARLY = 3
+            CUL_LATER = 4
+
+            for row in csv_reader:
+                print('for dynasty {}, {} => {}'.format(row[DYN], row[CUL_EARLY], row[CUL_LATER]))
 
         char_hist.rewrite(args.output_history_dir)  # Fully rewrite the history folder from parse trace in RAM
 
@@ -82,6 +103,10 @@ def main():
     except history.CHParseError as e:
         sys.stderr.write('\nFatal character history parse error:\n' + str(e))
         return 2
+
+    except csv.Error:
+        sys.stderr.write('\nFatal error while reading input CSV spreadsheet\n')
+        return 3
 
     except:
         sys.stderr.write('\nUnexpected fatal error occurred! Stack trace:\n\n')
