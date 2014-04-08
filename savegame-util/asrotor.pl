a239ee8a55007f9558640a28267590a6ad9d5d4a
Matthew D. Hall <zijistark@gmail.com>
2014-04-07 22:54:47 -0700

    Full benchmarking support and general extensibility added to asrotor

--- savegame-util/asrotor.pl
+++ savegame-util/asrotor.pl
@@ -7,35 +7,74 @@ use Carp;
 use Getopt::Long qw(:config gnu_getopt);
 use File::stat;
 use File::Copy;
+use File::Basename;
+use File::Spec;
 use Readonly;
 
+my $PROG = basename($0);
 
-Readonly my $LIVE_DIR => '/cygdrive/c/Users/Stark/Documents/Paradox Interactive/Crusader Kings II/HIP_ztrulers/save games';
-Readonly my $ARCHIVE_BASE_DIR => '/cygdrive/c/Users/Stark/Documents/asrotor';
+my $prog_no_ext = $PROG;
+$prog_no_ext =~ s/^\.//;
+$prog_no_ext =~ s/^(.*?)\..*$/$1/;
 
-croak "autosave monitoring directory not found: $LIVE_DIR" unless (-d $LIVE_DIR);
-croak "archive base directory not found: $ARCHIVE_BASE_DIR" unless (-d $ARCHIVE_BASE_DIR);
+my $ARCHIVE_DIR_DEFAULT = "/cygdrive/c/Users/$ENV{USER}/Documents/$prog_no_ext";
+$ARCHIVE_DIR_DEFAULT = undef unless -d $ARCHIVE_DIR_DEFAULT;
 
+my $USER_DIR_DEFAULT = "/cygdrive/c/Users/$ENV{USER}/Documents/Paradox Interactive/Crusader Kings II";
+$USER_DIR_DEFAULT = undef unless -d $USER_DIR_DEFAULT;
+
+
+my $opt_archive_dir = $ARCHIVE_DIR_DEFAULT;
+my $opt_user_dir = $USER_DIR_DEFAULT;
+my $opt_mod_user_dir;
 my $opt_name;
 my $opt_continue = 0;
+my $opt_bench_file = 0;
 
 GetOptions(
+	'a|archive-dir=s' => \$opt_archive_dir,
+	'u|user-dir=s' => \$opt_user_dir,
+	'm|mod-user-dir=s' => \$opt_mod_user_dir,
     'n|name=s' => \$opt_name,
-    'c|continue' => \$opt_continue)
-    or croak;
+    'c|continue' => \$opt_continue,
+	) or croak;
 
-croak "you must specify a name for the savegame series with --name" unless $opt_name;
+croak "specify a user directory with --user-dir" unless $opt_user_dir;
+croak "specify a name for the savegame series with --name" unless $opt_name;
+croak "specify an archive root directory with --archive-dir" unless $opt_archive_dir;
 
-my $autosave_file = "$LIVE_DIR/autosave.ck2";
-my $archive_dir = "$ARCHIVE_BASE_DIR/$opt_name";
+croak "user directory not found: $opt_user_dir" unless -d $opt_user_dir;
+croak "archive base directory not found: $opt_archive_dir" unless -d $opt_archive_dir;
+
+my $user_dir = $opt_user_dir;
+
+if ($opt_mod_user_dir) {
+	$user_dir = File::Spec->catdir($opt_user_dir, $opt_mod_user_dir);
+	
+	unless (-e $user_dir) {
+		mkdir $user_dir or croak "folder creation failed: $!: $user_dir";
+	}
+}
+
+my $save_dir = "$user_dir/save games";
+my $autosave_file = "$save_dir/autosave.ck2";
+my $archive_dir = "$opt_archive_dir/$opt_name";
 my $counter_file = "$archive_dir/.counter";
+my $bench_file = "$archive_dir/benchmark_$opt_name.csv";
 
+unless (-e $save_dir) {
+	mkdir $save_dir or croak "folder creation failed: $!: $save_dir";
+}
+
+my $bf;
 my $time_start = time();
 my $time_last = $time_start;
 my $counter_start = 0;
 my $counter = 0;
 
 sub finish {
+	$bf->close;
+
 	print "Archived ".($counter-$counter_start)." autosaves, series totaling $counter, over ".sprintf("%.2f", (time()-$time_start)/60)." minutes.\n";
 	print "Path: $archive_dir\n";
 	exit 0;
@@ -50,22 +89,29 @@ if (-e $archive_dir) {
 
     read_counter_file();
 	$counter_start = $counter;
+	open($bf, '>>', $bench_file) or croak "file open failed: $!: $bench_file";
 }
 else {
     mkdir($archive_dir) or croak $!;
     update_counter_file();
+	open($bf, '>', $bench_file) or croak "file open failed: $!: $bench_file";
+	$bf->print("Relative Year;Duration (seconds);File Size (MB)\n");
 }
 
 my $as_mtime = (-f $autosave_file) ? stat($autosave_file)->mtime : 0;
+my $waiting = 1;
 
 while (1) {
-	
+
+	if ($waiting) {
+		print STDERR "Waiting for first autosave (start the game)...\n";
+		$waiting = 0; # only print this reminder at the start
+	}
+
 	my $st = stat($autosave_file);
 	
 	if (defined $st && $st->mtime > $as_mtime) {
 		# autosave file is present and its mtime is newer than previously recorded
-		
-		$as_mtime = $st->mtime;
 		
 		# sleep an extra 15 seconds to allow for the game to do a slow write-out
 		# of a very large save.  we may otherwise catch the file in the middle of
@@ -73,11 +119,29 @@ while (1) {
 		
 		sleep(15);
 		
-		# now move the save to the head of our archive series
-
+		# redo the stat, in case the mtime is later now due to an in-progress write
+		# (and, if benchmarking, we want the exact time between the end-of-write
+		# of autosaves)
+		$st = stat($autosave_file);
+		my $size_mb = sprintf('%0.1f', $st->size / 1_000_000);
+	
+		if ($counter == $counter_start) { # the first save in a run can't be clocked
+			$bf->print($counter, ';', ';', $size_mb, "\n");
+		}
+		else {
+			$bf->print($counter, ';', $st->mtime-$as_mtime, ';', $size_mb, "\n");
+		}
+		
+		$bf->flush;
+		$as_mtime = $st->mtime; # rotate the previous mtime
+		
+		# and we now update the series index (for benchmark purposes, an interval of
+		# a year is assumed, and the offsets start at 0; hence, only updating now)
 		++$counter;
 		update_counter_file();
 		
+		# now move the save to the head of our archive series
+				
 		my $dest_file = "$opt_name.$counter.ck2";
 		File::Copy::move($autosave_file, "$archive_dir/$dest_file") or croak $!;
 		
