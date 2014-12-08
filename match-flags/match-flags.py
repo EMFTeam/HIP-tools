d44a3bb729ca8fc15ac970c07968514295bd5881
Matthew D. Hall <zijistark@gmail.com>
2014-12-07 20:42:12 -0800

    Added CoA <-> landed_titles matcher/checker tool

---
+++ match-flags/match-flags.py
@@ -0,0 +1,88 @@
+__author__ = 'zijistark'
+VERSION = '1.0'
+
+import sys
+import os
+import traceback
+import argparse
+import re
+
+DEFAULT_VANILLA_DIR = "C:/Program Files (x86)/Steam/SteamApps/common/Crusader Kings II/gfx/flags"
+p_flag_file = re.compile(r'^(.*?)\.tga$')
+p_title_all = re.compile(r'\W([cdek]_[\w_\-]+)')
+p_title = re.compile(r'^[cdek]_[\w_\-]+')
+p_txt_file = re.compile(r'^.*?\.txt$')
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Ensure all CK2 landed_titles have a CoA.")
+    parser.add_argument('--flags', '-f', default='./gfx/flags',
+                        help='path to primary flag directory [default: %(default)s]')
+    parser.add_argument('--flags-fallback', default=DEFAULT_VANILLA_DIR,
+                        help='path to output file [default: %(default)s]')
+    parser.add_argument('--titles', '-t', default='./common/landed_titles',
+                        help='path to landed_titles directory [default: %(default)s]')
+    parser.add_argument('--verbose', '-v', action='count', default=0,
+                        help='show verbose information about what the script is doing;'
+                             'repeat this option multiple times for more details')
+    parser.add_argument('--version', '-V', action='version', version='%(prog)s '+VERSION,
+                        help='show program version and quit')
+    return parser.parse_args()
+
+
+# noinspection PyBroadException
+
+
+def main():
+
+    args = get_args()
+
+    try:
+        if not os.path.isdir(args.flags):
+            sys.stderr.write('Primary flag directory invalid: {}\n'.format(args.flags))
+            return 1
+        if not os.path.isdir(args.flags_fallback):
+            sys.stderr.write('Vanilla (fallback) flag directory invalid: {}\n'.format(args.flags_fallback))
+            return 1
+        if not os.path.isdir(args.titles):
+            sys.stderr.write('Landed titles directory invalid: {}\n'.format(args.titles))
+            return 1
+
+        flag_set = set()
+
+        for f in os.listdir(args.flags_fallback) + os.listdir(args.flags):
+            m = p_flag_file.match(f)
+            if m is None:
+                print("Filename does not match expected pattern (*.tga): {}".format(f))
+            else:
+                flag_set.add(m.group(1))
+
+        print("Flag set: {}".format(flag_set))
+
+        title_set = set()
+
+        for f in os.listdir(args.titles):
+            if p_txt_file.match(f):
+                with open(os.path.join(args.titles, f)) as in_file:
+                    data = in_file.read()
+                    matches = []
+                    m = p_title.match(data)
+                    if m:
+                        matches = [m.group(0)]
+                    matches += p_title_all.findall(data)
+                    print("Matches from {}:\n{}\n".format(f, matches))
+                    for t in matches:
+                        title_set.add(t)
+        title_set -= flag_set
+
+        for t in title_set:
+            print(t)
+
+        return 0
+
+    except:
+        sys.stderr.write("\nFATAL ERROR:\n")
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(255)
+
+if __name__ == "__main__":
+    sys.exit(main())
--- pdxhist/scanner.cc
+++ pdxhist/scanner.cc
@@ -12,7 +12,7 @@
 #define FLEX_SCANNER
 #define YY_FLEX_MAJOR_VERSION 2
 #define YY_FLEX_MINOR_VERSION 5
-#define YY_FLEX_SUBMINOR_VERSION 35
+#define YY_FLEX_SUBMINOR_VERSION 39
 #if YY_FLEX_SUBMINOR_VERSION > 0
 #define FLEX_BETA
 #endif
@@ -145,15 +145,7 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k.
- * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
- * Ditto for the __ia64__ case accordingly.
- */
-#define YY_BUF_SIZE 32768
-#else
 #define YY_BUF_SIZE 16384
-#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -165,7 +157,12 @@ typedef unsigned int flex_uint32_t;
 typedef struct yy_buffer_state *YY_BUFFER_STATE;
 #endif
 
-extern int yyleng;
+#ifndef YY_TYPEDEF_YY_SIZE_T
+#define YY_TYPEDEF_YY_SIZE_T
+typedef size_t yy_size_t;
+#endif
+
+extern yy_size_t yyleng;
 
 extern FILE *yyin, *yyout;
 
@@ -187,6 +184,13 @@ extern FILE *yyin, *yyout;
                     if ( yytext[yyl] == '\n' )\
                         --yylineno;\
             }while(0)
+    #define YY_LINENO_REWIND_TO(dst) \
+            do {\
+                const char *p;\
+                for ( p = yy_cp-1; p >= (dst); --p)\
+                    if ( *p == '\n' )\
+                        --yylineno;\
+            }while(0)
     
 /* Return all but the first "n" matched characters back to the input stream. */
 #define yyless(n) \
@@ -203,11 +207,6 @@ extern FILE *yyin, *yyout;
 	while ( 0 )
 
 #define unput(c) yyunput( c, (yytext_ptr)  )
-
-#ifndef YY_TYPEDEF_YY_SIZE_T
-#define YY_TYPEDEF_YY_SIZE_T
-typedef size_t yy_size_t;
-#endif
 
 #ifndef YY_STRUCT_YY_BUFFER_STATE
 #define YY_STRUCT_YY_BUFFER_STATE
@@ -226,7 +225,7 @@ struct yy_buffer_state
 	/* Number of characters read into yy_ch_buf, not including EOB
 	 * characters.
 	 */
-	int yy_n_chars;
+	yy_size_t yy_n_chars;
 
 	/* Whether we "own" the buffer - i.e., we know we created it,
 	 * and can realloc() it to grow it, and should free() it to
@@ -296,8 +295,8 @@ static YY_BUFFER_STATE * yy_buffer_stack = 0; /**< Stack as an array. */
 
 /* yy_hold_char holds the character lost when yytext is formed. */
 static char yy_hold_char;
-static int yy_n_chars;		/* number of characters read into yy_ch_buf */
-int yyleng;
+static yy_size_t yy_n_chars;		/* number of characters read into yy_ch_buf */
+yy_size_t yyleng;
 
 /* Points to current character in buffer. */
 static char *yy_c_buf_p = (char *) 0;
@@ -325,7 +324,7 @@ static void yy_init_buffer (YY_BUFFER_STATE b,FILE *file  );
 
 YY_BUFFER_STATE yy_scan_buffer (char *base,yy_size_t size  );
 YY_BUFFER_STATE yy_scan_string (yyconst char *yy_str  );
-YY_BUFFER_STATE yy_scan_bytes (yyconst char *bytes,int len  );
+YY_BUFFER_STATE yy_scan_bytes (yyconst char *bytes,yy_size_t len  );
 
 void *yyalloc (yy_size_t  );
 void *yyrealloc (void *,yy_size_t  );
@@ -357,7 +356,7 @@ void yyfree (void *  );
 
 /* Begin user sect3 */
 
-#define yywrap(n) 1
+#define yywrap() 1
 #define YY_SKIP_YYWRAP
 
 typedef unsigned char YY_CHAR;
@@ -502,7 +501,7 @@ char *yytext;
 /* basic startup scanner for reacclimating myself with flex */
 #define YY_NO_UNISTD_H 1
 
-#line 506 "<stdout>"
+#line 505 "<stdout>"
 
 #define INITIAL 0
 
@@ -541,7 +540,7 @@ FILE *yyget_out (void );
 
 void yyset_out  (FILE * out_str  );
 
-int yyget_leng (void );
+yy_size_t yyget_leng (void );
 
 char *yyget_text (void );
 
@@ -583,12 +582,7 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k */
-#define YY_READ_BUF_SIZE 16384
-#else
 #define YY_READ_BUF_SIZE 8192
-#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -689,11 +683,6 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 20 "scanner.ll"
-
-
-#line 696 "<stdout>"
-
 	if ( !(yy_init) )
 		{
 		(yy_init) = 1;
@@ -720,6 +709,12 @@ YY_DECL
 		yy_load_buffer_state( );
 		}
 
+	{
+#line 20 "scanner.ll"
+
+
+#line 717 "<stdout>"
+
 	while ( 1 )		/* loops until end-of-file is reached */
 		{
 		yy_cp = (yy_c_buf_p);
@@ -736,7 +731,7 @@ YY_DECL
 yy_match:
 		do
 			{
-			register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
+			register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)] ;
 			if ( yy_accept[yy_current_state] )
 				{
 				(yy_last_accepting_state) = yy_current_state;
@@ -762,7 +757,7 @@ yy_find_action:
 
 		if ( yy_act != YY_END_OF_BUFFER && yy_rule_can_match_eol[yy_act] )
 			{
-			int yyl;
+			yy_size_t yyl;
 			for ( yyl = 0; yyl < yyleng; ++yyl )
 				if ( yytext[yyl] == '\n' )
 					   
@@ -845,7 +840,7 @@ YY_RULE_SETUP
 #line 34 "scanner.ll"
 YY_FATAL_ERROR( "flex scanner jammed" );
 	YY_BREAK
-#line 849 "<stdout>"
+#line 844 "<stdout>"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -977,6 +972,7 @@ case YY_STATE_EOF(INITIAL):
 			"fatal flex scanner internal error--no action found" );
 	} /* end of action switch */
 		} /* end of scanning one token */
+	} /* end of user's declarations */
 } /* end of yylex */
 
 /* yy_get_next_buffer - try to read in a new buffer
@@ -1032,21 +1028,21 @@ static int yy_get_next_buffer (void)
 
 	else
 		{
-			int num_to_read =
+			yy_size_t num_to_read =
 			YY_CURRENT_BUFFER_LVALUE->yy_buf_size - number_to_move - 1;
 
 		while ( num_to_read <= 0 )
 			{ /* Not enough room in the buffer - grow it. */
 
 			/* just a shorter name for the current buffer */
-			YY_BUFFER_STATE b = YY_CURRENT_BUFFER;
+			YY_BUFFER_STATE b = YY_CURRENT_BUFFER_LVALUE;
 
 			int yy_c_buf_p_offset =
 				(int) ((yy_c_buf_p) - b->yy_ch_buf);
 
 			if ( b->yy_is_our_buffer )
 				{
-				int new_size = b->yy_buf_size * 2;
+				yy_size_t new_size = b->yy_buf_size * 2;
 
 				if ( new_size <= 0 )
 					b->yy_buf_size += b->yy_buf_size / 8;
@@ -1077,7 +1073,7 @@ static int yy_get_next_buffer (void)
 
 		/* Read in more data. */
 		YY_INPUT( (&YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move]),
-			(yy_n_chars), (size_t) num_to_read );
+			(yy_n_chars), num_to_read );
 
 		YY_CURRENT_BUFFER_LVALUE->yy_n_chars = (yy_n_chars);
 		}
@@ -1172,7 +1168,7 @@ static int yy_get_next_buffer (void)
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 	yy_is_jam = (yy_current_state == 33);
 
-	return yy_is_jam ? 0 : yy_current_state;
+		return yy_is_jam ? 0 : yy_current_state;
 }
 
     static void yyunput (int c, register char * yy_bp )
@@ -1187,7 +1183,7 @@ static int yy_get_next_buffer (void)
 	if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
 		{ /* need to shift things up to make room */
 		/* +2 for EOB chars. */
-		register int number_to_move = (yy_n_chars) + 2;
+		register yy_size_t number_to_move = (yy_n_chars) + 2;
 		register char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
 					YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
 		register char *source =
@@ -1240,7 +1236,7 @@ static int yy_get_next_buffer (void)
 
 		else
 			{ /* need more input */
-			int offset = (yy_c_buf_p) - (yytext_ptr);
+			yy_size_t offset = (yy_c_buf_p) - (yytext_ptr);
 			++(yy_c_buf_p);
 
 			switch ( yy_get_next_buffer(  ) )
@@ -1517,7 +1513,7 @@ void yypop_buffer_state (void)
  */
 static void yyensure_buffer_stack (void)
 {
-	int num_to_alloc;
+	yy_size_t num_to_alloc;
     
 	if (!(yy_buffer_stack)) {
 
@@ -1614,12 +1610,12 @@ YY_BUFFER_STATE yy_scan_string (yyconst char * yystr )
  * 
  * @return the newly allocated buffer state object.
  */
-YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, int  _yybytes_len )
+YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, yy_size_t  _yybytes_len )
 {
 	YY_BUFFER_STATE b;
 	char *buf;
 	yy_size_t n;
-	int i;
+	yy_size_t i;
     
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
@@ -1701,7 +1697,7 @@ FILE *yyget_out  (void)
 /** Get the length of the current token.
  * 
  */
-int yyget_leng  (void)
+yy_size_t yyget_leng  (void)
 {
         return yyleng;
 }
