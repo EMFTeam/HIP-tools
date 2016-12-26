22d324eb24373c03e0aed5b67dd2a3467d643ae6
Nicholas Escalona <nicholas.escalona@gmail.com>
2016-12-26 16:51:28 -0500

    sed2 rebuilding, null-delimited git, disabled code for restart, pythonisms

--- hiphub/hiphub.py
+++ hiphub/hiphub.py
@@ -50,6 +50,10 @@ class TooManyRetriesException(Exception):
         return 'Retried command too many times'
 
 
+class SourceModifiedException(Exception):
+    pass
+
+
 def fatal(msg):
     sys.stderr.write('fatal: ' + msg + '\n')
     sys.stderr.flush()
@@ -59,7 +63,7 @@ def fatal(msg):
 def git_run(args, retry=False):
     cmd = [str(g_gitbin_path)] + args
     n_tries = 0
-    
+
     while True:
         cp = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         n_tries += 1
@@ -73,7 +77,7 @@ def git_run(args, retry=False):
                 cp.check_returncode()  # will throw for us
             elif n_tries == 1000:
                 raise TooManyRetriesException()
-            
+
             # TODO: finish this once we know a) what git will typically give us on stderr vs. stdout,
             # and b) what the errors for transient network issues tend to look like, as we only want to
             # to do "infinite" retry for those. [until then, setup errors and the like are just going to
@@ -89,13 +93,13 @@ def update_head(repo, branch):
     git_run(['reset', '--hard', 'HEAD'])
     git_run(['clean', '-f'])
 
-    # now checkout the desired branch and pull 
+    # now checkout the desired branch and pull
     git_run(['checkout', branch])
     git_run(['pull'], retry=True)
 
     # determine the head's possibly-new SHA
     cp = git_run(['rev-parse', 'HEAD'])
-    
+
     os.chdir(str(g_base_dir))
     return cp.stdout.strip()
 
@@ -103,29 +107,25 @@ def update_head(repo, branch):
 def git_files_changed(repo, branch, old_rev, new_rev='HEAD'):
     os.chdir(str(g_root_repo_dir / repo))
     git_run(['checkout', branch])
-    cp = git_run(['log', '--name-only', '--pretty=format:', '{}..{}'.format(old_rev, new_rev)])
-    changed_files = set()
-
-    for line in cp.stdout.splitlines():
-        if line != '':
-            changed_files.add(Path(line))
+    cp = git_run(['log', '--pretty=format:', '-z', '--name-only', '{}..{}'.format(old_rev, new_rev)])
+    changed_files = {Path(path) for path in cp.stdout.split('\x00') if path}
 
     os.chdir(str(g_base_dir))
     return changed_files
 
-    
+
 def has_this_repo_changed(ignored_file=None):
-    cp = git_run(['status', '--porcelain'])
-    p_line = re.compile(r'^\s*\S+\s(.+)$')  ## example line: '?? i got mad spaces and am untracked, yo.txt'
-    for line in cp.stdout.splitlines():
-        m = p_line.match(line)
-        if m:
-            path = m.group(1)
-            if ignored_file is None or path != ignored_file:
-                return True
+    cp = git_run(['status', '-z'])  # filenames are terminated with NUL instead of line break
+    status_iter = iter(cp.stdout.split('\x00')[:-1])  ## example entry: '?? i got mad spaces and am untracked, yo.txt'
+    for entry in status_iter:
+        if entry[0] == 'R':
+            next(status_iter)  # there is an extra filename (what path was renamed from). skip it
+        path = entry[3:]
+        if ignored_file is None or path != ignored_file:
+            return True
     return False
 
-                    
+
 def load_state():
     global g_ignored_rev
     g_ignored_rev = {}
@@ -140,7 +140,7 @@ def load_state():
                 with p.open() as f:
                     g_last_rev[h] = f.read().strip()
 
-                    
+
 # assuming repo is SWMH-BETA and we've processed SWMH-BETA before
 # (s.t. we have a list of files changed), should we rebuild MiniSWMH?
 def should_rebuild_mini_from_swmh(branch, changed_files):
@@ -149,23 +149,24 @@ def should_rebuild_mini_from_swmh(branch, changed_files):
                       'SWMH/map/default.map',
                       'SWMH/map/definition.csv']
 
-    for p in specific_paths:
-        if Path(p) in changed_files:
-            return True
-    p_wanted_file = re.compile(r'^SWMH/history/(?:titles|provinces)/.+?\.txt$')
-    for f in changed_files:
-        if p_wanted_file.match(str(f)):
-            return True
-    return False
+    if any(Path(p) in changed_files for p in specific_paths):
+        return True
+    p_wanted_file = r'^SWMH/history/(?:titles|provinces)/.+?\.txt$'
+    return any(re.match(p_wanted_file, str(f)) for f in changed_files)
+
+
+def should_rebuild_sed_from_swmh(branch, changed_files):
+    landed_titles = Path('SWMH/common/landed_titles')
+    return any(landed_titles in p.parents for p in changed_files)
 
 
 def rebuild_mini(swmh_branch):
     logging.info('rebuilding MiniSWMH...')
     # get on the right branches
-    mini_branch = 'master' if swmh_branch == 'master' else 'timeline'
+    mini_branch = g_repos['MiniSWMH'][g_repos['SWMH-BETA'].index(swmh_branch)]
     os.chdir(str(g_root_repo_dir / 'MiniSWMH'))
     git_run(['checkout', mini_branch])
-    
+
     # SWMH is already on the right branch. for something more complex
     # like a `rebuild_sed2`, we'd have to be more careful about this.
 
@@ -195,7 +196,7 @@ def rebuild_mini(swmh_branch):
 
     # determine the head's new SHA so that we may ignore it for future processing
     g_ignored_rev['MiniSWMH:' + mini_branch] = git_run(['rev-parse', 'HEAD']).stdout.strip()
-    
+
     # .. and puuuuush, deep breaths
     git_run(['push'], retry=True)
 
@@ -203,12 +204,53 @@ def rebuild_mini(swmh_branch):
     logging.info('rebuild of MiniSWMH resulted in net change, so pushed new MiniSWMH.')
     os.chdir(str(g_base_dir))
     return True
-    
-                    
+
+
+def rebuild_sed2(swmh_branch):
+    logging.info('rebuilding SED...')
+    # get on the right branches
+    sed_branch = g_repos['sed2'][g_repos['SWMH-BETA'].index(swmh_branch)]
+    os.chdir(str(g_root_repo_dir / 'sed2'))
+    git_run(['checkout', sed_branch])
+
+    # SWMH is already on the right branch.
+    cp = subprocess.run(['./build.py'], stderr=subprocess.PIPE, shell=True, universal_newlines=True)
+
+    if cp.returncode != 0:
+        logging.error('failed to rebuild SED:\n>command: {}\n>code: {}\n>error:\n{}\n'.format(cp.args, cp.returncode, cp.stderr))
+        git_run(['reset', '--hard', 'HEAD'])
+        git_run(['clean', '-f'])
+        os.chdir(str(g_base_dir))
+        return False
+
+    # did anything change besides our version.txt?
+    version_file = 'build/SED2/version.txt'
+    if not has_this_repo_changed(ignored_file=version_file):
+        # eh, no biggie-- cleanup version.txt and go
+        git_run(['checkout', version_file])
+        os.chdir(str(g_base_dir))
+        return False
+
+    # if we're here, we do indeed have changes to commit.
+    git_run(['add', '-A'])
+    git_run(['commit', '-a', '-m', 'rebuild from upstream changes :robot_face:'])
+
+    # determine the head's new SHA so that we may ignore it for future processing
+    g_ignored_rev['sed2:' + sed_branch] = git_run(['rev-parse', 'HEAD']).stdout.strip()
+
+    # .. and puuuuush, deep breaths
+    git_run(['push'], retry=True)
+
+    # ta-dah!
+    logging.info('rebuild of sed2 resulted in net change, so pushed new sed2.')
+    os.chdir(str(g_base_dir))
+    return True
+
+
 def process_head_change(repo, branch, head_rev):
     head = '{}:{}'.format(repo, branch)
     do_processing = True
-    
+
     if g_ignored_rev[head] is head_rev:
         logging.debug('processing skipped for rev due to being self-emitted: %s/%s [%s]', repo, branch, head_rev)
         do_processing = False
@@ -217,7 +259,7 @@ def process_head_change(repo, branch, head_rev):
 
     if do_processing:
         build_mini = False
-        build_sed = False  # not implemented yet
+        build_sed = False
 
         if repo == 'SWMH-BETA':
             if head not in g_last_rev:  # first time (all files in repo changed, effectively)
@@ -226,21 +268,29 @@ def process_head_change(repo, branch, head_rev):
             else:
                 changed_files = git_files_changed(repo, branch, g_last_rev[head])
                 build_mini = should_rebuild_mini_from_swmh(branch, changed_files)
-                
+                build_sed = should_rebuild_sed_from_swmh(branch, changed_files)
+
             if build_mini:
                 rebuild_mini(branch)
+            if build_sed:
+                rebuild_sed2(branch)
+
+        elif repo == 'HIP-tools':
+            changed_files = git_files_changed(repo, branch, g_last_rev[head])
+            if Path('hiphub/hiphub.py') in changed_files:
+                pass #raise SourceModifiedException()  # XXX
 
     # update memory state
     g_last_rev[head] = head_rev
 
     # update state on disk
-    with (g_state_dir / head).open("w") as f:
+    with (g_state_dir / head).open('w') as f:
         print(head_rev, file=f)
 
 
 def init_daemon():
     # mark our tracks with pidfile
-    with g_pidfile_path.open("w") as f:
+    with g_pidfile_path.open('w') as f:
         print(os.getpid(), file=f)
 
     # deprioritize our process scheduling priority (as well as our subprocesses')
@@ -255,12 +305,12 @@ def init_daemon():
     os.environ['PYTHONPATH'] = str(g_root_repo_dir / 'ck2utils/esc')
     os.environ['USER'] = g_daemon_user
     os.environ['HOME'] = '/home/' + g_daemon_user
-    
+
     load_state()
 
     logging.debug('updating all tracked heads...')
     proc_needed = []
-    
+
     for repo in g_repos:
         for branch in g_repos[repo]:
             rev = update_head(repo, branch)
@@ -269,7 +319,7 @@ def init_daemon():
                 proc_needed.append( (repo, branch, rev) )
 
     # TODO: might want to sort proc_needed so that, e.g., SWMH-BETA < MiniSWMH < EMF
-    
+
     for pn in proc_needed:
         process_head_change(*pn)
 
@@ -284,7 +334,7 @@ def run_daemon():
             p = g_webhook_dir / h
             if p.exists():
                 last_mtime[h] = p.stat().st_mtime
-    
+
     while True:
         time.sleep(1)  # one-second polling interval
 
@@ -336,8 +386,8 @@ def shutdown_daemon(exit_code=0):
             p.kill()
     except psutil.NoSuchProcess:
         pass
-    
-    g_pidfile_path.unlink()    
+
+    g_pidfile_path.unlink()
     sys.exit(exit_code)
 
 
@@ -346,7 +396,7 @@ def kill_existing_daemon():
     if not g_pidfile_path.exists():
         return
 
-    # get the PID of the running hiphub daemon    
+    # get the PID of the running hiphub daemon
     with g_pidfile_path.open() as f:
         pid = int(f.read().strip())
 
@@ -359,7 +409,7 @@ def kill_existing_daemon():
 
     n_tries = 0
     need_hard_kill = False
-    
+
     while True:
         time.sleep(0.1)
         n_tries += 1
@@ -374,15 +424,28 @@ def kill_existing_daemon():
         os.kill(pid, signal.SIGKILL)
         if g_pidfile_path.exists():
             g_pidfile_path.unlink()
-                
+
+
+def restart_program(context, owd):
+    os.chdir(owd)
+    context.close()
+    kill_existing_daemon()
+    sys.stdout.flush()
+    sys.stderr.flush()
+    try:
+        os.execvp('python3', [s for s in sys.argv if s != '--restart'])
+    except OSError as e:
+        logging.error(e)
+
 
 def sig_terminate_handler(sig_num, stack_frame):
     shutdown_daemon()
 
-    
+
 def main():
     opt_restart = '--restart' in sys.argv[1:]
-    
+    owd = os.getcwd()
+
     context = daemon.DaemonContext()
     context.working_directory = str(g_base_dir)
 
@@ -405,7 +468,7 @@ def main():
     }
 
     context.umask = 0o002
-        
+
     with context:
         # initialize logging
         logging.basicConfig(filename=str(g_logfile_path),
@@ -418,9 +481,12 @@ def main():
             logging.info('hiphub v{} starting with pid {}...'.format(VERSION, os.getpid()))
             init_daemon()
             run_daemon()
+        except SourceModifiedException as e:
+            logging.warning('hiphub.py modified, hiphub will restart...')
+            restart_program(context, owd)
         except Exception as e:
             # will direct a traceback to the log
-            logging.exception("unhandled exception, hiphub must terminate:")
+            logging.exception('unhandled exception, hiphub must terminate:')
             shutdown_daemon(exit_code=255)
 
 
